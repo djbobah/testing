7aa33b5155e7c0c05211a7ba614ad829bf169ffe
Vladimir Kirpichev <djbobah@mail.ru>
2023-03-06 21:24:04 +0300

    forms1 14 done

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -52,7 +52,7 @@ const Login = () => {
     <div className="container mt-5">
       <div className="row">
         <div className="col-md-6 offset-md-3 shadow p-4">
-          <h3 className="mb-3">Login</h3>
+          <h3 className="mb-4">Login</h3>
           <form onSubmit={handleSubmit}>
             <TextField
               label="Электронная почта:"
--- src/components/textField.jsx
+++ src/components/textField.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TextField = ({ label, type, name, value, onChange, error }) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const getInputClasses = () => {
+    return "form-control " + (error ? "is-invalid" : "");
+  };
+  const toggleShowPassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
   return (
-    <div>
+    <div className="mb-4">
       <label htmlFor={name}>{label}</label>
-      <input
-        type={type}
-        id={name}
-        name={name}
-        value={value}
-        onChange={onChange}
-      />
-      {error && <p>{error}</p>}
+      <div className="input-group has-validation">
+        <input
+          type={showPassword ? "text" : type}
+          id={name}
+          name={name}
+          value={value}
+          onChange={onChange}
+          className={getInputClasses()}
+        />
+        {type === "password" && (
+          <button
+            className="btn btn-outline-secondary"
+            onClick={toggleShowPassword}
+          >
+            <i className={"bi bi-eye" + (showPassword ? "-slash" : "")}></i>
+          </button>
+        )}
+        {error && <div className="invalid-feedback">{error}</div>}
+      </div>
     </div>
   );
 };
