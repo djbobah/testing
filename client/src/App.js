a6c1975c8668bbf07603802d364da7783e444e9b
Kirpichev Vladimir <djbobah@mail.ru>
2023-04-18 16:48:32 +0300

    finish server routs, add navbar, comment fields in login and register forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
+import NavBar from "./components/navBar";
 import Login from "./layouts/login";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Routes>
         <Route path="/">
           <Route path=":type?" element={<Login />} />
